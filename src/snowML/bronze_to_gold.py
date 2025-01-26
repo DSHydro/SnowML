903276616e21b62a8a72c91e5277636b0e3220eb
sue-t-boyd <155676297+sue-t-boyd@users.noreply.github.com>
2025-01-26 09:36:46 -0800

    Sue4 (#7)
    * Simplify get_geos
    
    * update get_geos; add additional wrf vars to pipeline

--- src/snowML/bronze_to_gold.py
+++ src/snowML/bronze_to_gold.py
@@ -1,26 +1,26 @@
-""" Module to download raw climate data saved as a zarr file
-    into csv files clipeed by geo"""
+"""Module to calculate daily mean of climate data and save as a csv file"""
+
 
 # pylint: disable=C0103
 
 import importlib
 import time
-import warnings
 import xarray as xr
 import data_utils as du
 import set_data_constants as sdc
 
-modules_to_reload = [sdc]
+modules_to_reload = []
 for m in modules_to_reload:
     importlib.reload(m)
 
 # define constants
-BUCKET_DICT = sdc.create_bucket_dict("prod")
 VAR_DICT = sdc.create_var_dict()
 
 
+def prep_bronze(geo, var, bucket_dict = None):
+    if bucket_dict is None:
+        bucket_dict = sdc.create_bucket_dict("prod")
 
-def prep_bronze(geos, var, bucket_dict = BUCKET_DICT):
     # load_raw
     if var == "swe":
         b_bronze = bucket_dict.get(f"{var}-bronze")
@@ -30,13 +30,12 @@ def prep_bronze(geos, var, bucket_dict = BUCKET_DICT):
     ds = xr.open_zarr(store=zarr_store_url, consolidated=True)
 
     # Perform first cut crude filter
-    min_lon, min_lat, max_lon, max_lat = geos.unary_union.bounds
+    min_lon, min_lat, max_lon, max_lat = geo.bounds
     small_ds = du.crude_filter(ds, min_lon, min_lat, max_lon, max_lat)
 
     # Sort if necessary
     if not small_ds['lat'].to_index().is_monotonic_increasing:
-        small_ds = small_ds.sortby("lat")  # Only sort if required
-
+        small_ds = small_ds.sortby("lat")
 
     if var != "swe":
         transform = du.calc_transform(small_ds)
@@ -47,68 +46,80 @@ def prep_bronze(geos, var, bucket_dict = BUCKET_DICT):
     return small_ds
 
 
-def process_silver_row (small_ds, row):
-    ds_filter = du.filter_by_geo (small_ds, row)
-    silver_df = ds_filter.to_dataframe()
-    silver_df = silver_df[silver_df.columns.drop(["spatial_ref", "crs"])]
-    huc_id = row.iloc[0, 1] # get the id of the smaller huc unit
-    silver_df["huc_id"] = huc_id
-    return silver_df
+def create_mask(ds, row, crs):
+    # Create a mask for the geometry
+    mask = ds.rio.clip([row.geometry], crs, drop=False, invert=False)
+    return mask
 
-def process_gold (silver_df, var, huc_id):
+# def view_slice(ds):
+#     time_slice = slice('1996-12-01', '1996-12-02')
+#     ds_subset = ds.sel(time=time_slice)
+#     print(ds_subset)
+#     df = ds_subset.to_dataframe()
+#     df.dropna(subset=['SWE'], inplace=True)
+#     print(df)
 
+
+def ds_to_gold(ds, var):
     if var == "swe":
-        grouper = "time"
         var_name = "SWE"
+        daily_mean = ds[var_name].resample(time='D').mean(dim=['lat', 'lon'])
+        daily_mean = daily_mean.assign_coords(time=ds['time'].resample(time='D').first())
+        daily_mean = daily_mean.to_dataset(name=f"mean_{var_name}")
+        daily_mean['time'] = ds['time']
     else:
-        grouper = "day"
         var_name = VAR_DICT.get(var)
-    gold_df = silver_df.groupby([grouper])[var_name].mean().reset_index() # TO DO: Fix Logic
-    gold_df["huc_id"] = huc_id
-    gold_df = gold_df.rename(columns={var_name: f"mean_{var_name}"})
-    return gold_df
+        if not ds['day'].to_index().is_monotonic_increasing:
+            ds = ds.sortby("day")
+        daily_mean = ds[var_name].resample(day='D').mean(dim=['lat', 'lon'])
+        daily_mean = daily_mean.assign_coords(day=ds['day'].resample(day='D').first())
+        daily_mean = daily_mean.to_dataset(name=f"mean_{var_name}")
+        daily_mean['day'] = ds['day']
 
+    return daily_mean
 
-def process_all(geos, var, bucket_dict=BUCKET_DICT, save_sil = False, overwrite = False):
+def bronze_to_gold (geos, var, bucket_dict = None, overwrite = False):
     time_start = time.time()
-    num_hucs = geos.shape[0]
+    crs = geos.crs
+    var_name = VAR_DICT.get(var)
+    if bucket_dict is None:
+        bucket_dict = sdc.create_bucket_dict("prod")
 
-    # get and prep bronze data
-    with warnings.catch_warnings():
-        warnings.filterwarnings("ignore", category=FutureWarning)  # TO DO: ADDRESS THE FUTURE WARNING
-        small_ds = prep_bronze(geos, var, bucket_dict)
-
-    # silver and gold processing
-    gold_df_list = []
-    for i in range (geos.shape[0]):
-        row = geos.iloc[[i]]
-        huc_id = row.iloc[0, 1] # get the id of the smaller huc uit
-        print(f"Processing {var} for huc_id {huc_id}, {i+1} of {num_hucs}")
-        # silver processing
-        f_silver = f"raw_{var}_in_{huc_id}"
-        if var == "swe":
-            b_silver = bucket_dict.get(f"{var}-silver")
-        else:
-            b_silver = bucket_dict.get("wrf-silver")
-        if du.isin_s3(b_silver, f"{f_silver}.csv") and not overwrite:
-            print(f"File{f_silver} already exists in {b_silver}")
-            silver_df = du.s3_to_df(f"{f_silver}.csv", b_silver)
-        else:
-            silver_df = process_silver_row(small_ds, row)
-            if save_sil:
-                du.dat_to_s3(silver_df, b_silver, f_silver, file_type="csv")
+    # Loop through each geometry in the GeoDataFrame
+    for idx, row in geos.iterrows():
+        huc_id = row['huc_id']  # Extract the huc_id for naming
+        print(f"Processing huc {idx+1} of {geos.shape[0]}, huc_id: {huc_id}")
 
 
-        # gold_processing
+        # process to silver
+        geo = row.geometry  # Extract the geometry
+        small_ds = prep_bronze(geo, var, bucket_dict=bucket_dict)
+        df_silver = create_mask(small_ds, row, crs)
+        # TO DO - ADD A SAVE GATE FOR SILVER
+
+        # process to gold
         f_gold = f"mean_{var}_in_{huc_id}"
         if var == "swe":
             b_gold = bucket_dict.get(f"{var}-gold")
         else:
             b_gold = bucket_dict.get("wrf-gold")
+
         if du.isin_s3(b_gold, f"{f_gold}.csv") and not overwrite:
             print(f"File{f_gold} already exists in {b_gold}")
         else:
-            gold_df = process_gold(silver_df, var, huc_id)
-            gold_df_list.append(gold_df)
+            ds_gold = ds_to_gold(df_silver, var)
+            du.elapsed(time_start)
+            #ds_gold = ds_gold.chunk({'time': -1})
+            #s3_path = f"mean_{var}_in{huc_id}.zarr"
+            #ds_gold.to_zarr(f"s3://{b_gold}/{s3_path}", mode="w", consolidated=True)
+            gold_df = ds_gold.to_dataframe()
+            gold_df["huc_id"] = huc_id
+            gold_df = gold_df[gold_df.columns.drop("crs")]
+            if var in ["tmmn", "rmin"]:
+                gold_df = gold_df.rename(columns={var_name: f"mean_{var_name}_min"})
+            elif var in ["tmmx", "rmax"]:
+                gold_df = gold_df.rename(columns={var_name: f"mean_{var_name}_max"})
+            else: 
+                gold_df = gold_df.rename(columns={var_name: f"mean_{var_name}"})
             du.dat_to_s3(gold_df, b_gold, f_gold, file_type="csv")
-        du.elapsed(time_start)
+            du.elapsed(time_start)

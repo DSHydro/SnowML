9b6cc97acd1d26912ca193b4b11efdfca2b94633
sue-t-boyd <suetboyd@uw.edu>
2025-01-27 14:25:32 -0800

    revert sdc

--- src/snowML/set_data_constants.py
+++ src/snowML/set_data_constants.py
@@ -3,18 +3,17 @@
 def create_bucket_dict(b_type):
     permitted_types = ["prod", "test"]
     assert b_type in permitted_types, f"Type must be one of {permitted_types}"
-    bucket_types = ["shape-bronze", "swe-bronze",  "swe-silver", "swe-gold", \
-                "wrf-bronze", "wrf-silver", "wrf-gold", "model-ready", "sues-test"]
+    # bucket_types = ["shape-bronze", "bronze",  "silver", "gold",  "model-ready"]
+    bucket_types = ["shape-bronze", "swe-bronze",  "swe-silver", "swe-gold", "wrf-bronze", "wrf-silver", "wrf-gold", "model-ready"]
     if b_type == "prod":
+        # bucket_names = ["shape-bronze", "dawgs-bronze", "dawgs-silver", \
+        #                 "dawgs-gold", "dawgs-model-ready"]
         bucket_names = bucket_types
         bucket_dict = dict(zip(bucket_types, bucket_names))
-        bucket_dict['model-ready'] = 'dawgs-model-ready'
-        return bucket_dict
-    # if b_type is not prod, then b_type == "test"
-    bucket_names = ["sues-scratch"] * len(bucket_types)
-    bucket_dict = dict(zip(bucket_types, bucket_names))
-    #bucket_dict['swe-bronze'] = 'swe-bronze'
-    #bucket_dict['wrf-bronze'] = 'wrf-bronze'
+        bucket_dict["model-ready"] = "dawgs-model-ready"
+    else: # if b_type is not prod, then b_type == "test"
+        bucket_names = ["sues-scratch"] * len(bucket_types)
+        bucket_dict = dict(zip(bucket_types, bucket_names))
     return bucket_dict
 
 def create_var_dict():

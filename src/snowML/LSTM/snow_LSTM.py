d6d12ca06e4d64410e2d21339efcc1505102840d
sue-t-boyd <suetboyd@uw.edu>
2025-02-13 20:29:21 -0800

    Update LSTM

--- src/snowML/LSTM/snow_LSTM.py
+++ src/snowML/LSTM/snow_LSTM.py
@@ -1,9 +1,11 @@
 # pylint: disable=C0103
 
 import random
+import os
 import torch
 from torch import nn
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import mlflow
 from sklearn.metrics import mean_squared_error
@@ -53,49 +55,35 @@ def create_dataloader(df, params):
 # Pre-training Phase: Train on multiple HUCs
 def pre_train(model, optimizer, loss_fn, df_dict, params):
     """ Pre-train the model on multiple HUCs """
-
-    # Calculate the number of pre-train epochs
-    pre_train_epochs = int(params['n_epochs'] * params['pre_train_fraction'])
-
     # Initialize available keys for sampling without replacement
-    available_keys = [key for key in df_dict.keys()]
-    random.shuffle(available_keys)  # Shuffle for randomness
 
-    for epoch in range(pre_train_epochs):
-        model.train()  # Set model to training mode
-        print(f"Epoch {epoch}: Pre-training on multiple HUCs")
-        random.shuffle(available_keys)
+    available_keys = list(df_dict.keys())
+    random.shuffle(available_keys)
+
+    model.train()  # Set model to training mode
 
 
-        # Iterate over all the HUCs and train on them
-        for i, selected_key in enumerate(available_keys, start=1):
-            if i % 5 == 0:
-                print(f"Epoch {epoch}: Pre-training on HUC{i}: {selected_key}")
+    for i, selected_key in enumerate(available_keys, start=1):
+        if i % 5 == 0:
+            print(f"Pre-training on HUC{i}: {selected_key}")
 
-            loader = create_dataloader(
-                df_dict[selected_key],
-                params
+        loader = create_dataloader(
+            df_dict[selected_key],
+            params
             )
 
-            # Training Loop
-            for X_batch, y_batch in loader:
-                optimizer.zero_grad()
-                y_pred = model(X_batch)
-                loss = loss_fn(y_pred, y_batch)
-                loss.backward()
-                optimizer.step()
+        # Training Loop
+        for X_batch, y_batch in loader:
+            optimizer.zero_grad()
+            y_pred = model(X_batch)
+            loss = loss_fn(y_pred, y_batch)
+            loss.backward()
+            optimizer.step()
 
-
-        # Perform validation every 5 epochs; use the most recent selected key
-        df_validation = df_dict[selected_key]
-        if epoch % 5 == 0:
-            validate_model(model, loss_fn, df_validation, params["var_list"], params['lookback'])
 
 # Fine-tuning Phase: Train on target HUC
-def fine_tune(model, optimizer, loss_fn, df_dict, target_key, params):
-    """ Fine-tune the model on the target HUC """
+def fine_tune(model, optimizer, loss_fn, df_dict, target_key, params, epoch):
 
-    n_epochs = int(params['n_epochs']*(1-params['pre_train_fraction']))
     df_target = df_dict[target_key]
 
     # Create DataLoader for fine-tuning (target HUC)
@@ -104,56 +92,125 @@ def fine_tune(model, optimizer, loss_fn, df_dict, target_key, params):
         params
         )
 
-    for epoch in range(n_epochs):
-        model.train()  # Set model to training mode
-        print(f"Epoch {epoch}: Fine-tuning on target HUC {target_key}")
+    model.train()  # Set model to training mode
+    print(f"Epoch {epoch}: Fine-tuning on target HUC {target_key}")
 
-        # Training Loop on target HUC
-        for X_batch, y_batch in loader:
-            optimizer.zero_grad()
-            y_pred = model(X_batch)
-            loss = loss_fn(y_pred, y_batch)
-            loss.backward()
-            optimizer.step()
-
-        # Perform validation every 5 epochs
-        if epoch % 5 == 0:
-            validate_model(model, loss_fn, df_target, params["var_list"], params['lookback'])
+    # Training Loop on target HUC
+    for X_batch, y_batch in loader:
+        optimizer.zero_grad()
+        y_pred = model(X_batch)
+        loss = loss_fn(y_pred, y_batch)
+        loss.backward()
+        optimizer.step()
 
 
-def validate_model(model, loss_fn, df_target, var_list, lookback):
-    """ Performs validation on target datasets """
-    model.eval()
+def kling_gupta_efficiency(y_true, y_pred):
+    r = np.corrcoef(y_true.ravel(), y_pred.ravel())[0, 1] # Correlation coefficient
+    alpha = np.std(y_pred) / np.std(y_true)  # Variability ratio
+    beta = np.mean(y_pred) / np.mean(y_true)  # Bias ratio
+    kg = 1 - np.sqrt((r - 1)**2 + (alpha - 1)**2 + (beta - 1)**2)
+    #print(f"r: {r}, alpha: {alpha}, beta: {beta}")
+    return kg, r, alpha, beta
+
+def store_metrics(metric_names, metrics_list_dict, df_dict, epoch):
+    df = pd.DataFrame({
+        metric_names[0]: metrics_list_dict[metric_names[0]],
+        metric_names[1]: metrics_list_dict[metric_names[1]],
+        metric_names[2]: metrics_list_dict[metric_names[2]],
+        metric_names[3]: metrics_list_dict[metric_names[3]]
+        }, index=df_dict.keys())
+
+    # Compute the mean for each metric 
+    if len(df) > 1:
+        mean_values = df.mean()
+        print("Mean Metrics:")
+        print(mean_values)
+        # Log each mean metric in mlflow
+        for metric_name, mean_value in mean_values.items():
+            mlflow.log_metric(f"mean_{metric_name}", mean_value, step = epoch)
+
+    # Add a total row
+    df.loc['Total'] = mean_values
+
+    # Log the dataframe in mlflow
+    csv_path = f"results_epoch{epoch}.csv"
+    df.to_csv(csv_path, index=True)
+    mlflow.log_artifact(csv_path)
+    os.remove(csv_path)
+
+    # Log the dataframe in mlflow
+    csv_path = f"results_epoch{epoch}.csv"
+    df.to_csv(csv_path, index=True)
+    mlflow.log_artifact(csv_path)
+    os.remove(csv_path)
+
+def predict(model_dawgs, df_dict, selected_key, params):
+    data = df_dict[selected_key]
+    train_main, test_main, train_size_main, _  = pp.train_test_split(data, params['train_size_fraction'])
+    X_train, y_train = pp.create_tensor(train_main,
+                                        params['lookback'],
+                                        params['var_list'])
+
+    X_test, y_test = pp.create_tensor(test_main,
+                                    params['lookback'],
+                                    params['var_list'])
     with torch.no_grad():
-        # Evaluate on target dataset
-        X_val_target, y_val_target = pp.create_tensor(df_target, lookback, var_list)
-        y_pred_target = model(X_val_target)
-        val_rmse_target = np.sqrt(loss_fn(y_pred_target, y_val_target))
-        print(f"Validation RMSE on most recently trained huc: {val_rmse_target:.4f}")
+        y_train_pred = model_dawgs(X_train).cpu().numpy()
+        y_test_pred = model_dawgs(X_test).cpu().numpy()
+        y_train = y_train.numpy()
+        y_test = y_test.numpy()
 
-def predict(data, model, X_train, X_test, train_size, huc_id, params):
-    data = data.astype(object)
-    with torch.no_grad():
-        # Initialize empty arrays with NaNs for plotting
-        train_plot = np.full_like(data['mean_swe'].values, np.nan, dtype=float)
-        test_plot = np.full_like(data['mean_swe'].values, np.nan, dtype=float)
+    return data, y_train_pred, y_test_pred, y_train, y_test, train_size_main
 
-        # Ensure X_train and X_test are tensors
-        if isinstance(X_train, np.ndarray):
-            X_train = torch.tensor(X_train, dtype=torch.float32)
-        if isinstance(X_test, np.ndarray):
-            X_test = torch.tensor(X_test, dtype=torch.float32)
 
-        # Predict on training data
-        y_pred = model(X_train)
-        y_pred_new = y_pred[:, -1].unsqueeze(1)
 
-        # Convert tensor to numpy safely
-        train_plot[params["lookback"]:train_size] = y_pred_new.detach().cpu().numpy().flatten()
+def evaluate(model_dawgs, df_dict, params, epoch, selected_keys = None):
+    if selected_keys is None:
+        available_keys = list(df_dict.keys())
+        random.shuffle(available_keys)
+    else: 
+        available_keys = selected_keys
+    metric_names = ["train_mse", "test_mse", "train_kge", "test_kge"]
+    metrics_list_dict = {metric: [] for metric in metric_names}
 
-        # Predict on test data
-        y_pred_test = model(X_test)[:, -1].unsqueeze(1)
-        test_plot[train_size + params["lookback"] : len(data)] = y_pred_test.detach().cpu().numpy().flatten()
+    # Loop through each HUC
+    for selected_key in available_keys:
+
+        data, y_train_pred, y_test_pred, y_train_true, y_test_true, train_size_main = predict(model_dawgs, df_dict, selected_key, params)
+
+        # Compute MSE
+        train_mse = mean_squared_error(y_train_true, y_train_pred)
+        test_mse = mean_squared_error(y_test_true, y_test_pred)
+
+        # Compute KGE
+        train_kge, _, _, _ = kling_gupta_efficiency(y_train_true, y_train_pred)
+        test_kge, _, _, _ = kling_gupta_efficiency(y_test_true, y_test_pred)
+
+        metrics = [train_mse, test_mse, train_kge, test_kge]
+
+
+        # Log, print, & save metrics
+        print("")
+        for i in range(len(metrics)):
+            mlflow.log_metric(f"{metric_names[i]}_{str(selected_key)}", metrics[i], step=epoch)
+            print(f"{metric_names[i]}_{str(selected_key)}: {metrics[i]}")
+            metrics_list_dict[metric_names[i]].append(metrics[i])
+
+         # store plots for final epooch
+        if epoch == params["n_epochs"] - 1:
+            plot(data, y_train_pred, y_test_pred, train_size_main, selected_key, params)
+
+    store_metrics(metric_names, metrics_list_dict, df_dict, epoch)
+
+
+def plot(data, y_train_pred, y_test_pred, train_size, huc_id, params):
+
+    train_plot = np.full_like(data['mean_swe'].values, np.nan, dtype=float)
+    test_plot = np.full_like(data['mean_swe'].values, np.nan, dtype=float)
+
+    # Convert tensor to numpy safely
+    train_plot[params["lookback"]:train_size] = y_train_pred.flatten()
+    test_plot[train_size + params["lookback"] : len(data)] = y_test_pred.flatten()
 
     # plot
     plt.figure(figsize=(12,  6))
@@ -172,52 +229,4 @@ def predict(data, model, X_train, X_test, train_size, huc_id, params):
     #plt.savefig(f"{ttle}.png")
     mlflow.log_figure(plt.gcf(), ttle+".png")
     plt.close()
-
-def kling_gupta_efficiency(y_true, y_pred):
-    r = np.corrcoef(y_true.ravel(), y_pred.ravel())[0, 1] # Correlation coefficient
-    alpha = np.std(y_pred) / np.std(y_true)  # Variability ratio
-    beta = np.mean(y_pred) / np.mean(y_true)  # Bias ratio
-    kg = 1 - np.sqrt((r - 1)**2 + (alpha - 1)**2 + (beta - 1)**2)
-    #print(f"r: {r}, alpha: {alpha}, beta: {beta}")
-    return kg, r, alpha, beta
-
-def evaluate_metrics(model, X_train, y_train, X_test, y_test, target_key, step_value):
-
-    with torch.no_grad():
-        y_train_pred = model(X_train).cpu().numpy()
-        y_test_pred = model(X_test).cpu().numpy()
-        y_train_pred = model(X_train).numpy()
-        y_test_pred = model(X_test).numpy()
-        y_train_true = y_train.numpy()
-        y_test_true = y_test.numpy()
-
-        # check for NaNs
-        # for arr, name in [(y_train_pred, "y_train_pred"),
-        #                           (y_test_pred, "y_test_pred"),
-        #                           (y_train_true, "y_train_true"),
-        #                           (y_test_true, "y_test_true")]:
-        #             print(f"{name}: NaNs: {np.isnan(arr).any()}, Infs: {np.isinf(arr).any()}")
-
-
-        # Compute MSE
-        train_mse = mean_squared_error(y_train_true, y_train_pred)
-        test_mse = mean_squared_error(y_test_true, y_test_pred)
-
-        # Compute KGE
-        train_kge, _, _, _ = kling_gupta_efficiency(y_train_true, y_train_pred)
-        test_kge, _, _, _ = kling_gupta_efficiency(y_test_true, y_test_pred)
-
-        # Log metrics
-        mlflow.log_metric(f"train_mse_{str(target_key)}", train_mse, step=step_value)
-        mlflow.log_metric(f"test_mse_{str(target_key)}", test_mse, step=step_value)
-        mlflow.log_metric(f"train_kge_{str(target_key)}", train_kge, step=step_value)
-        mlflow.log_metric(f"test_kge_{str(target_key)}", test_kge, step=step_value)
-
-        # Print metrics
-        print(f"train_mse_{str(target_key)}: {train_mse}")
-        print(f"test_mse_{str(target_key)}: {test_mse}")
-        print(f"train_kge_{str(target_key)}: {train_kge}")
-        print(f"test_kge_{str(target_key)}: {test_kge}")
-
-        return (train_mse, test_mse, train_kge, test_kge)
     

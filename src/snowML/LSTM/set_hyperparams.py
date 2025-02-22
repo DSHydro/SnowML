c7284028f6791f84ba4f3e8bcde1ceda72b0c7c2
sue-t-boyd <suetboyd@uwe.edu>
2025-02-22 02:46:25 +0000

    remove ephemeral bug fix

--- src/snowML/LSTM/set_hyperparams.py
+++ src/snowML/LSTM/set_hyperparams.py
@@ -11,17 +11,17 @@ def create_hyper_dict():
         "dropout": 0.5,
         "learning_rate": 1e-3,  # 3e-3
         "n_epochs": 10,
-        "pre_train_fraction" : 0,
-        "train_size_fraction": .67,
-        "train_size_dimension": "time",
+        "pre_train_fraction" : 1,
+        "train_size_fraction": .8,
+        "train_size_dimension": "huc",
         "lookback": 180,
         "batch_size": 64,
         "n_steps": 1,
         "num_workers": 8,
         "var_list": ["mean_pr", "mean_tair"],
         #"var_list": ["mean_pr", "mean_tair", "Maritime", "Ephemeral", "Montane Forest", "Ice"],
-        "expirement_name": "MarineMulti2", 
-        "input_pairs": [[17110005, '12'], [17110006, '12'], [17110009, '12'], [1702009, '12']],
+        "expirement_name": "Maritime_Multi_2", 
+        "input_pairs": [[17110005, '12'], [17110006, '12'], [17110009, '12'], [17020009, '12']],
         "exclude_ephem": True, 
     }
     return param_dict

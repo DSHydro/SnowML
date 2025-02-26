eda74c5e0b27f9481f52a8c8f31f549bb5a259a2
sue-t-boyd <suetboyd@uw.edu>
2025-02-25 22:44:38 -0800

    update preprocess

--- src/snowML/LSTM/LSTM_pre_process.py
+++ src/snowML/LSTM/LSTM_pre_process.py
@@ -1,5 +1,6 @@
 
 # pylint: disable=C0103
+# module to assemble the test, validation, and training data for the LSTM module
 
 import pandas as pd
 import numpy as np
@@ -9,47 +10,6 @@ from snowML.datapipe import get_geos as gg
 from snowML.datapipe import set_data_constants as sdc
 from snowML.datapipe import snow_types as st
 
-# Excluded Hucs Due to Missing SWE Data (Canada)
-EXCLUDED_HUCS = ["1711000501", "1711000502", "1711000503", "171100050101", "171100050102", \
-                "171100050201", "171100050202", "171100050203", "171100050301", \
-                "171100050302", "171100050303", "171100050304", "171100050305", \
-                "171100050306", "171100050401"] 
-
-
-
-def assemble_huc_list(params):
-    """
-    Assembles a list of HUC (Hydrologic Unit Code) IDs from a list of input pairs.
-
-    Args:
-        input_pairs (list of tuples): A list of tuples where each tuple contains two elements:
-            - The first element is a string representing the huc code for the region of interest.
-            - The second element is a string or intrepresenting the lowest huc subunit to study.
-
-    Returns:
-        list: HUC IDs extracted from the geojson files corresponding to the input pairs.
-
-    Example:
-        input_pairs = [("RegionA", "10"), ("RegionB", "12")]
-        huc_list = assemble_huc_list(input_pairs)
-    """
-    hucs = []
-    input_pairs = params["input_pairs"]
-    for pair in input_pairs:
-        geos = gg.get_geos(pair[0], pair[1])
-        if params["exclude_ephem"] is True:
-            geos = snowclass_filter(geos)
-            print(f"filtered shape for {pair[0]} is {geos.shape}")
-        hucs.extend(geos["huc_id"].to_list())
-    return hucs
-
-# function that filters geos to exlcude hucs where predominant snowtype is ephemeral
-def snowclass_filter(geos):
-    df_snow_types = st.snow_class(geos)
-    # Filter huc_ids where Ephemeral < 50
-    valid_huc_ids = df_snow_types.loc[df_snow_types["Ephemeral"] < 50, "huc_id"]
-    geos_filtered = geos[geos["huc_id"].isin(valid_huc_ids)]
-    return geos_filtered
 
 
 def z_score_normalize(df, global_means, global_stds):
@@ -87,21 +47,20 @@ def pre_process(huc_list, var_list, bucket_dict=None):
 
     # Step 1: Load all dataframes and collect them for global mean and std computation
     for huc in huc_list:
-        if huc not in EXCLUDED_HUCS:
-            file_name = f"model_ready_huc{huc}.csv"
-            df = du.s3_to_df(file_name, bucket_name)
-            df['day'] = pd.to_datetime(df['day'])
-            df.set_index('day', inplace=True)  # Set 'day' as the index
-            # Collect only the columns of interest
-            col_to_keep = var_list + ["mean_swe"]
+        file_name = f"model_ready_huc{huc}.csv"
+        df = du.s3_to_df(file_name, bucket_name)
+        df['day'] = pd.to_datetime(df['day'])
+        df.set_index('day', inplace=True)  # Set 'day' as the index
+        # Collect only the columns of interest
+        col_to_keep = var_list + ["mean_swe"]
 
-            for col in col_to_keep:
-                if col not in df.columns:
-                    print(f"huf{huc} is missing col {col}")
+        for col in col_to_keep:
+            if col not in df.columns:
+                print(f"huf{huc} is missing col {col}")
 
-            df = df[col_to_keep]
-            all_dfs.append(df)  # Collect DataFrames for global normalization
-            df_dict[huc] = df  # Store DataFrame in dictionary
+        df = df[col_to_keep]
+        all_dfs.append(df)  # Collect DataFrames for global normalization
+        df_dict[huc] = df  # Store DataFrame in dictionary
 
     # Step 2: Calculate global mean and std for each column of interest across all HUCs
     combined_df = pd.concat(all_dfs)
@@ -113,29 +72,8 @@ def pre_process(huc_list, var_list, bucket_dict=None):
         df = z_score_normalize(df, global_means, global_stds)  # Pass global mean and std for normalization
         df_dict[huc] = df  # Store normalized DataFrame
 
-    print(f"number of sub units for pre-training is {len(df_dict)}")
+    print(f"number of sub units for training is {len(df_dict)}")
     return df_dict
-
-def train_test_split(data, train_size_fraction):
-    """
-    Splits the given data into training and testing sets based on the specified fraction.
-
-    Parameters:
-    data (list or array-like): The dataset to be split.
-    train_size_fraction (float): The fraction of the data to be used for the training set. 
-                                 Should be a value between 0 and 1.
-
-    Returns:
-    tuple: A tuple containing:
-        - train_main (list or array-like): The training set.
-        - test_main (list or array-like): The testing set.
-        - train_size_main (int): The size of the training set.
-        - test_size_main (int): The size of the testing set.
-    """
-    train_size_main = int(len(data) * train_size_fraction)
-    test_size_main = len(data) - train_size_main
-    train_main, test_main = data[:train_size_main], data[train_size_main:]
-    return train_main, test_main, train_size_main, test_size_main
 
 def create_tensor(dataset, lookback, var_list):
     """Transform the time series into a tensor object.

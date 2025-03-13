f0e68afb28acf7c507e14721b48a6ff4b450b622
sue-t-boyd <suetboyd@uwe.edu>
2025-03-13 19:04:59 +0000

    Rationalize file structure

---
+++ src/snowML/Scripts/load_huc_splits.py
@@ -0,0 +1,16 @@
+""" Moduel that loads and returns train, validate, and test hucs from a saved json file"""
+
+import json
+
+
+def huc_split(f="src/snowML/Scripts_Ex3/hucs_data.json"):
+    with open(f, 'r') as file:
+        data = json.load(file)
+    tr = data["train_hucs"]
+    #print(tr[0:4])
+    val = data["val_hucs"]
+    #print(val[0:4])
+    te = data["test_hucs"]
+    #print(te[0:4])
+    return tr, val, te
+    

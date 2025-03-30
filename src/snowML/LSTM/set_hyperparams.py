4b48c07fc41311f108eed1171297acf3f8a80451
sue-t-boyd <suetboyd@uw.edu>
2025-03-30 18:15:29 +0000

    update metrics and mixed loss training script

--- src/snowML/LSTM/set_hyperparams.py
+++ src/snowML/LSTM/set_hyperparams.py
@@ -9,13 +9,13 @@ def create_hyper_dict():
         "num_class": 1,
         "num_layers": 1,
         "dropout": 0.5,
-        "learning_rate": 3e-4,  # 3e-3
-        "n_epochs": 30,
+        "learning_rate": 1e-3,  # 3e-3
+        "n_epochs": 10,
         "lookback": 180,
-        "batch_size": 32,
+        "batch_size": 64,
         "n_steps": 1,
         "num_workers": 8,
-        "var_list": ["mean_pr", "mean_tair", "mean_hum"],
+        "var_list": ["mean_pr", "mean_tair"],
         "expirement_name": "Hybrid-Loss",
         "loss_type": "hybrid",
         "mse_lambda_start": 1, 

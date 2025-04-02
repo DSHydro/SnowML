314fe3bdfdc887a58afe163333678993fce5a52d
sue-t-boyd <suetboyd@uw.edu>
2025-04-02 06:13:22 +0000

    add data integration functionality

--- src/snowML/LSTM/set_hyperparams.py
+++ src/snowML/LSTM/set_hyperparams.py
@@ -9,14 +9,14 @@ def create_hyper_dict():
         "num_class": 1,
         "num_layers": 1,
         "dropout": 0.5,
-        "learning_rate":  3e-4, #1e-3,  # 3e-3
+        "learning_rate":  3e-4, #1e-3, # 3e-3
         "n_epochs": 10,
         "lookback": 180,
         "batch_size": 32,
         "n_steps": 1,
         "num_workers": 8,
-        "var_list": ["mean_pr", "mean_tair"],
-        "expirement_name": "Maritime_Redo",
+        "var_list": ["mean_pr", "mean_tair", "mean_swe_lag_30"],
+        "expirement_name": "Data_Integration",
         "loss_type": "mse",
         "mse_lambda_start": 1, 
         "mse_lambda_end": 0, 
